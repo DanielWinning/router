cb4d628d205a8cce6bbf63c459cd102a6cded367
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-02-09 01:06:14 +0000

    test case names

--- tests/DataProvider/RouteDataProvider.php
+++ tests/DataProvider/RouteDataProvider.php
@@ -10,13 +10,13 @@ class RouteDataProvider
     public static function invalidControllerArrayProvider(): \Generator
     {
         $cases = [
-            [[]],
-            [[1, 2]],
-            [[new \DateTime(), new \DateTime()]]
+            'empty array' => [[]],
+            'array of integers' => [[1, 2]],
+            'array of DateTime objects' => [[new \DateTime(), new \DateTime()]]
         ];
 
-        foreach ($cases as $case) {
-            yield $case;
+        foreach ($cases as $name => $case) {
+            yield $name => $case;
         }
     }
 
@@ -26,14 +26,14 @@ class RouteDataProvider
     public static function invalidRoutePathProvider(): \Generator
     {
         $cases = [
-            ['/hello{/world'],
-            ['/users/{id/1}'],
-            ['/users/?!/'],
-            ['//users//']
+            '/hello{/world' => ['/hello{/world'],
+            '/users/{id/1}' => ['/users/{id/1}'],
+            '/users/?!/' => ['/users/?!/'],
+            '//users//' => ['//users//']
         ];
 
-        foreach ($cases as $case) {
-            yield $case;
+        foreach ($cases as $name => $case) {
+            yield $name => $case;
         }
     }
 
@@ -43,13 +43,13 @@ class RouteDataProvider
     public static function dynamicPathProvider(): \Generator
     {
         $cases = [
-            ['/users/{id}'],
-            ['/users/{id}/edit'],
-            ['/posts/{category}/{post}']
+            '/users/{id}' => ['/users/{id}'],
+            '/users/{id}/edit' => ['/users/{id}/edit'],
+            '/posts/{category}/{post}' => ['/posts/{category}/{post}']
         ];
 
-        foreach ($cases as $case) {
-            yield $case;
+        foreach ($cases as $name => $case) {
+            yield $name => $case;
         }
     }
 
@@ -59,14 +59,14 @@ class RouteDataProvider
     public static function notDynamicPathProvider(): \Generator
     {
         $cases = [
-            ['/'],
-            ['/users'],
-            ['/posts/all'],
-            ['/contact-us/']
+            '/' => ['/'],
+            '/users' => ['/users'],
+            '/posts/all' => ['/posts/all'],
+            '/contact-us' => ['/contact-us/']
         ];
 
-        foreach ($cases as $case) {
-            yield $case;
+        foreach ($cases as $name => $case) {
+            yield $name => $case;
         }
     }
 }
